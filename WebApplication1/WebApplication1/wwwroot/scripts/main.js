fb36d1fcc2a5441f336fcaf6725aa94cd3aa44da
Housame <housame.oueslati@hotmail.com>
2017-12-23 01:55:55 +0100

    adding users to selectform using jquery done

--- WebApplication1/WebApplication1/wwwroot/scripts/main.js
+++ WebApplication1/WebApplication1/wwwroot/scripts/main.js
@@ -1,21 +1,32 @@
 ﻿$(document).ready(function () {
     console.log('Hello world');
+    
     var addUsers = $('#add-Users');
-
+    var usersSelect = $('#sel1');
+    //line: 15
+    getUsers();
+    //On clicking all users will be removed from db and re-added
     addUsers.on('click', function () {
         console.log("add users calles");
+
+        getUsers();
+    });
+        //Getting all users into logging select-form
+    function getUsers() {
         $.ajax({
             url: '/api/user/add',
             type: 'Get',
         }).done(function (result) {
             console.log('success');
-            getUsers();
+            console.log(result);
+            $.each(result, function (key, value) {
+                usersSelect
+                    .append($("<option></option>")
+                        .attr("value", value.id)
+                        .text(value.email));
+            })
         }).fail(function (xhr, status, error) {
             console.log('error');
         });
-    });
-
-    function getUsers() {
-        alert("users called!");
     }
 });
