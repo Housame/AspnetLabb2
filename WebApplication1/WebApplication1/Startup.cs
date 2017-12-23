777a6675bdeeede86eb1ed5ad267487761f95a58
Housame <housame.oueslati@hotmail.com>
2017-12-23 01:03:11 +0100

    first commit

---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RolesAndClaims.Entities;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, UserRole>()
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultTokenProviders();
+            services.AddMvc();
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AllowedToPublish", policy => policy.RequireRole("Publisher"));
+                options.AddPolicy("AllowedToView", policy => policy.RequireRole("Publisher", "Subscriber"));
+            });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new List<string> { "index.html" }
+            });
+            app.UseStaticFiles();
+            app.UseDirectoryBrowser();
+            app.UseStatusCodePages();
+            app.UseAuthentication();
+            app.UseMvc();
+
+
+
+        }
+    }
+}
