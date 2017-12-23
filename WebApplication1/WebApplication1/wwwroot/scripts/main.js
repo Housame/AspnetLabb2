7aeebf63188c38c45a4303e4f577860e007481a7
Housame <housame.oueslati@hotmail.com>
2017-12-23 01:15:02 +0100

    wwwroot updated with html/css/js files, dbcontext ....

---
+++ WebApplication1/WebApplication1/wwwroot/scripts/main.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    function test() {
+        $.ajax({
+            url: '/api/user/index',
+            type: 'Get',
+        }).done(function (result) {
+            console.log('test index');
+            console.log(result);
+        }).fail(function (xhr, status, error) {
+            console.log('test fail');
+        });
+    }
+    test();
+    function addroles() {
+        $.ajax({
+            url: '/api/user/addroles',
+            type: 'Post'
+        }).done(function (result) {
+            console.log('roles added');
+        }).fail(function (xhr, status, error) {
+            console.log('fail to add roles ');
+        });
+    }
+    addroles();
+    console.log('Hello world');
+    var addUsers = $('#add-Users');
+
+    addUsers.on('click', function () {
+        console.log("add users calles");
+        $.ajax({
+            url: '/api/user/add',
+            type: 'Get',
+        }).done(function (result) {
+            console.log('success');
+            getUsers();
+        }).fail(function (xhr, status, error) {
+            console.log('error');
+        });
+    });
+
+    function getUsers() {
+        alert("users called!");
+    }
+});
